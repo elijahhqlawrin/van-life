ba16f8b2f50feaf2518bb301456dd5801f0c12b1
elijahhqlawrin <129173452+elijahhqlawrin@users.noreply.github.com>
2023-09-26 12:59:58 -0300

    Add Firebase configuration obj

--- api.js
+++ api.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqyWX0jkYEOF4uECAzE0Ozbv3YZbfbnks",
+  authDomain: "vanlife-7ab15.firebaseapp.com",
+  projectId: "vanlife-7ab15",
+  storageBucket: "vanlife-7ab15.appspot.com",
+  messagingSenderId: "98001913898",
+  appId: "1:98001913898:web:cead8c5feddbb126871b53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
