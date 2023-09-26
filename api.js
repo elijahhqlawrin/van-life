7df634c605cb6313a98e799ebbe1bb93ef46bccd
elijahhqlawrin <129173452+elijahhqlawrin@users.noreply.github.com>
2023-09-26 14:34:11 -0300

    Refactor getHostVans
    Change func to fetch host van obj
    data if hostId matches hardcoded
    value

--- api.js
+++ api.js
@@ -4,7 +4,9 @@ import {
     collection, 
     doc, 
     getDoc, 
-    getDocs
+    getDocs, 
+    query, 
+    where
  } from "firebase/firestore/lite"
 
 const firebaseConfig = {
@@ -40,9 +42,18 @@ export async function getVan(id) {
     }
 }
 
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const hostVans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return hostVans
+}
 
-// export async function getVans(id) {
-//     const url = id ? `/api/vans/${id}` : "/api/vans"
+// export async function getHostVans(id) {
+//     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
 //     const res = await fetch(url)
 //     if (!res.ok) {
 //         throw {
@@ -54,20 +65,6 @@ export async function getVan(id) {
 //     const data = await res.json()
 //     return data.vans
 // }
-
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
